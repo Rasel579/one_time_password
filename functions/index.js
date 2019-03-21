cb26e38b4e8fa60de13d723fee906cf2a525ab1f
Ruslan <rasel579@gmail.com>
2019-03-21 13:39:08 +0500

    added settings for firebase, made a google function for creating a user using his phone number

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const createUser = require('./create_user');
+const serviceAccount = require('./service_account.json');
+const requestOneTimePassword = require('./request_one_time_password');
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://one-time-password-ad510.firebaseio.com"
+  });
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+
+
+exports.createUser = functions.https.onRequest(createUser);
+
